1eb2a3c7da9fbb2e880dd468486a549cada55304
mouthapongariya@gmail.com <mouthapongariya@gmail.com>
2026-03-23 01:29:38 -0400

    updated user model

--- backend/core/views.py
+++ backend/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, 'home.html')
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'challenge.html')
 
 from django.http import JsonResponse
 
--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# apparently this is supposed to like make it so
+# when someone posts smthn it like takes in
+# title body slug and then adds a date to it idk
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
