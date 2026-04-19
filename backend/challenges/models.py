f953129fb5b6f9d93481435af555ed56b3267b1a
kstauch <kaistauch123@hotmail.com>
2026-04-19 18:07:49 -0400

    Removed date field from Challenges table

--- backend/challenges/models.py
+++ backend/challenges/models.py
@@ -2,14 +2,11 @@ import uuid
 
 from django.db import models
 from django.conf import settings
-# Create your models here.
 
 class Challenges(models.Model):
     title = models.CharField(max_length=100)
     points = models.PositiveIntegerField(default=0)
     category = models.CharField(max_length=100)
-    date = models.DateField(auto_now_add=True)
-
 
     def __str__(self):
         return self.title
